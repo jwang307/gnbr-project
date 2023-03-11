1ac17363b3a85e45da83e74ccd1c43604813d289
Yash Patil <yash.s.patil125@gmail.com>
2023-03-11 14:34:56 -0800

    update

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -1,24 +1,20 @@
-from huggingface_hub import HfApi
+from tqdm import tqdm
+from transformers import BertTokenizerFast
 
-user_id = HfApi().whoami()["name"]
+# repositor id for saving the tokenizer
+tokenizer_id="biobert-tiny"
+# 20619956
+# create a python generator to dynamically load the data
+def batch_iterator(batch_size=10000):
+    for i in tqdm(range(0, len(10000), batch_size)):
+        temp = []
+        for x in range(0, 10000):
+            with open(f"./abstracts_v2/{i+x}.txt", "r") as text_file:
+                temp.append(text_file.read())
+        yield temp
 
-print(f"user id '{user_id}' will be used during the example")
+# create a tokenizer from existing one to re-use special tokens
+tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
 
-
-
-# from tqdm import tqdm
-# from transformers import BertTokenizerFast
-
-# # repositor id for saving the tokenizer
-# tokenizer_id="bert-base-uncased-2022-habana"
-
-# # create a python generator to dynamically load the data
-# def batch_iterator(batch_size=10000):
-#     for i in tqdm(range(0, len(raw_datasets), batch_size)):
-#         yield raw_datasets[i : i + batch_size]["text"]
-
-# # create a tokenizer from existing one to re-use special tokens
-# tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
-
-# bert_tokenizer = tokenizer.train_new_from_iterator(text_iterator=batch_iterator(), vocab_size=32_000)
-# bert_tokenizer.save_pretrained("tokenizer")
+bert_tokenizer = tokenizer.train_new_from_iterator(text_iterator=batch_iterator(), vocab_size=32_000)
+bert_tokenizer.save_pretrained("tokenizer")
