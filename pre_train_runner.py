f32e9f09a84a339d18460654c54f0ddbc1d33f1d
Yash Patil <yash.s.patil125@gmail.com>
2023-03-12 13:09:54 -0700

    update

--- pre_train_runner.py
+++ pre_train_runner.py
@@ -45,8 +45,7 @@ trainer = Trainer(
     model=model,
     args=training_args,
     data_collator=data_collator,
-    train_dataset=dataset,
-    prediction_loss_only=True,
+    train_dataset=dataset
 )
 
 # Start the training process
