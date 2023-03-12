da9f93edc51772ac973144e5f9142c04d28bea63
Yash Patil <yash.s.patil125@gmail.com>
2023-03-11 22:05:06 -0800

    update

--- pre_train.py
+++ pre_train.py
@@ -1,45 +1,67 @@
+import glob
 import multiprocessing
+import os
+from itertools import chain
 
+import tqdm
+from datasets import Dataset
+from huggingface_hub import HfApi
 from transformers import AutoTokenizer
 
-# load tokenizer
+user_id = HfApi().whoami()["name"]
+
+
+# specify the directory containing the files
+directory = "./abstracts_v2"
+
+# create an empty list to store the contents of each file
+file_contents = []
+
+# use the glob module to find all files in the directory
+files = glob.glob(os.path.join(directory, "*"))
+
+# loop over each file and read its contents
+for file in tqdm.tqdm(files):
+    with open(file, "r") as f:
+        content = f.read()
+        file_contents.append({ "text": content })
+
+raw_datasets = Dataset.from_list(file_contents)
+
 # tokenizer = AutoTokenizer.from_pretrained(f"{user_id}/{tokenizer_id}")
 tokenizer = AutoTokenizer.from_pretrained("tokenizer")
 num_proc = multiprocessing.cpu_count()
 print(f"The max length for the tokenizer is: {tokenizer.model_max_length}")
 
-# def group_texts(examples):
-#     tokenized_inputs = tokenizer(
-#        examples["text"], return_special_tokens_mask=True, truncation=True, max_length=tokenizer.model_max_length
-#     )
-#     return tokenized_inputs
+def group_texts(examples):
+    tokenized_inputs = tokenizer(
+       examples["text"], return_special_tokens_mask=True, truncation=True, max_length=tokenizer.model_max_length
+    )
+    return tokenized_inputs
 
-# # preprocess dataset
-# tokenized_datasets = raw_datasets.map(group_texts, batched=True, remove_columns=["text"], num_proc=num_proc)
-# tokenized_datasets.features
+# preprocess dataset
+tokenized_datasets = raw_datasets.map(group_texts, batched=True, remove_columns=["text"], num_proc=num_proc)
 
+def group_texts_2(examples):
+    # Concatenate all texts.
+    concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+    # customize this part to your needs.
+    if total_length >= tokenizer.model_max_length:
+        total_length = (total_length // tokenizer.model_max_length) * tokenizer.model_max_length
+    # Split by chunks of max_len.
+    result = {
+        k: [t[i : i + tokenizer.model_max_length] for i in range(0, total_length, tokenizer.model_max_length)]
+        for k, t in concatenated_examples.items()
+    }
+    return result
 
-# -------------------------------
+tokenized_datasets = tokenized_datasets.map(group_texts_2, batched=True, num_proc=num_proc)
+# shuffle dataset
+tokenized_datasets = tokenized_datasets.shuffle(seed=34)
 
-# import glob
-# import os
+print(f"the dataset contains in total {len(tokenized_datasets)*tokenizer.model_max_length} tokens")
 
-# import tqdm
-
-# # specify the directory containing the files
-# directory = "./abstracts_v2"
-
-# # create an empty list to store the contents of each file
-# file_contents = []
-
-# # use the glob module to find all files in the directory
-# files = glob.glob(os.path.join(directory, "*"))
-
-# # loop over each file and read its contents
-# for file in tqdm.tqdm(files):
-#     with open(file, "r") as f:
-#         content = f.read()
-#         file_contents.append(content)
-
-# # print the list of file contents
-# print(len(file_contents))
+dataset_id=f"{user_id}/processed_bert_dataset"
+tokenized_datasets.push_to_hub(f"{user_id}/processed_bio_bert_tiny_dataset")
