c9f53aa60bb1f32a5014376fbc6d0bd916c0e93a
Yash Patil <yash.s.patil125@gmail.com>
2023-03-11 21:11:41 -0800

    update

---
+++ pre_train.py
@@ -0,0 +1,20 @@
+import glob
+import os
+
+# specify the directory containing the files
+directory = "./abstracts_v2"
+
+# create an empty list to store the contents of each file
+file_contents = []
+
+# use the glob module to find all files in the directory
+files = glob.glob(os.path.join(directory, "*"))
+
+# loop over each file and read its contents
+for file in files:
+    with open(file, "r") as f:
+        content = f.read()
+        file_contents.append(content)
+
+# print the list of file contents
+print(len(file_contents))
