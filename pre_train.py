d9178eb822cce7730c9c4cf166fc2e7107f904e9
Yash Patil <yash.s.patil125@gmail.com>
2023-03-11 21:25:34 -0800

    update

--- pre_train.py
+++ pre_train.py
@@ -1,22 +1,45 @@
-import glob
-import os
+import multiprocessing
 
-import tqdm
+from transformers import AutoTokenizer
 
-# specify the directory containing the files
-directory = "./abstracts_v2"
+# load tokenizer
+# tokenizer = AutoTokenizer.from_pretrained(f"{user_id}/{tokenizer_id}")
+tokenizer = AutoTokenizer.from_pretrained("tokenizer")
+num_proc = multiprocessing.cpu_count()
+print(f"The max length for the tokenizer is: {tokenizer.model_max_length}")
 
-# create an empty list to store the contents of each file
-file_contents = []
+# def group_texts(examples):
+#     tokenized_inputs = tokenizer(
+#        examples["text"], return_special_tokens_mask=True, truncation=True, max_length=tokenizer.model_max_length
+#     )
+#     return tokenized_inputs
 
-# use the glob module to find all files in the directory
-files = glob.glob(os.path.join(directory, "*"))
+# # preprocess dataset
+# tokenized_datasets = raw_datasets.map(group_texts, batched=True, remove_columns=["text"], num_proc=num_proc)
+# tokenized_datasets.features
 
-# loop over each file and read its contents
-for file in tqdm.tqdm(files):
-    with open(file, "r") as f:
-        content = f.read()
-        file_contents.append(content)
 
-# print the list of file contents
-print(len(file_contents))
+# -------------------------------
+
+# import glob
+# import os
+
+# import tqdm
+
+# # specify the directory containing the files
+# directory = "./abstracts_v2"
+
+# # create an empty list to store the contents of each file
+# file_contents = []
+
+# # use the glob module to find all files in the directory
+# files = glob.glob(os.path.join(directory, "*"))
+
+# # loop over each file and read its contents
+# for file in tqdm.tqdm(files):
+#     with open(file, "r") as f:
+#         content = f.read()
+#         file_contents.append(content)
+
+# # print the list of file contents
+# print(len(file_contents))
